80c85b82d684353d473014581846bdb4587ea99b
ashim <ashimgautam01@gmail.com>
2025-02-24 11:30:07 +0545

    fix: update CORS origin to allow requests from the production URL

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ config()
 const PORT =process.env.PORT || 3000
 const app=express()
 app.use(cors({
-    origin:" http://localhost:5173",
+    origin:"https://payfront-peach.vercel.app",
     methods:["POST","GET","PATCH","PUT","DELETE"],
     credentials:true
 }))
